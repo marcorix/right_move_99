6dbb4f13c260be0a4b409c732bc3384a5e7c2c96
Marco Ricci <mr.ricci79@gmail.com>
2023-11-21 12:36:44 +0000

    new

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,9 +7,27 @@ class FlatsController < ApplicationController
 
   def show;end
 
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.user = current_user
+    if @flat.save!
+      redirect_to flats_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_flat
     @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :guests, :price, :property_type)
   end
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,3 +1,4 @@
 class Flat < ApplicationRecord
   belongs_to :user
+    TYPES = %w(flat maisonette detached)
 end
